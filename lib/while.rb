14f390f267408b38202a7c57450241687d2372f4
isabella g henriques <isabellagh88@gmail.com>
2020-03-30 04:29:36 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -8,10 +8,10 @@ end
 
 
 def using_until
-  
-counter = 0
-until counter == 20
-  puts "The current number is less than 20."
-  counter += 1
+  levitation_force = 6
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
 end
 
